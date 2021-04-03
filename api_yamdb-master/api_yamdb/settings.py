281103264c8605b64bc45f5ca1154a70b9eed67f
Руслан Алимов <rusichviz@gmail.com>
2021-04-03 18:54:52 +0300

    Change some models and make migrate

--- api_yamdb-master/api_yamdb/settings.py
+++ api_yamdb-master/api_yamdb/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'title.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- api_yamdb-master/title/models.py
+++ api_yamdb-master/title/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import AbstractUser
 
 
-User = get_user_model()
+class User(AbstractUser):
+    ROLE_CHOISE = (
+        ("user", "user"),
+        ("moderator", "moderator"),
+        ("admin", "admin")
+    )
+    role = models.CharField(choices=ROLE_CHOISE, default='user', max_length=10)
 
 
 class Category(models.Model):
@@ -19,23 +25,24 @@ class Genre(models.Model):
 class Title(models.Model):
     name = models.CharField(max_length=200)
     year = models.DateField()
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+
 
 class Genre_Title(models.Model):
-    title = models.ForeignKey(Title)
-    genre = models.ForeignKey(Genre)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
 
 class Review(models.Model):
-    title = models.ForeignKey(Title)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
     text = models.TextField()
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     score = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
     pub_date = models.DateTimeField("date_published", auto_now_add=True)
 
 
 class Comments(models.Model):
-    review = models.ForeignKey(Review)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
     text = models.TextField()
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField("date_published", auto_now_add=True)
